465bf535672a4271dce4447c606d5acaddd5fd2c
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-01-07 12:06:10 +0100

    added enter  prompts: action, file name

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -1,8 +1,17 @@
 package client;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Client client = new Client("127.0.0.1", 23456);
         client.start();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter action:");
+        String action = scanner.nextLine();
+
+        System.out.println("Enter filename:");
+        String filename = scanner.nextLine();
     }
 }
