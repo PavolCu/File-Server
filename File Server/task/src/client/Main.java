a4659f3be8ab0008a82f407e0e868c575d6afb8b
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-01-07 17:27:32 +0100

    added PUT, GET, DELETE in client/Main.java

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -12,6 +12,44 @@ public class Main {
         String action = scanner.nextLine();
 
         System.out.println("Enter filename:");
-        String filename = scanner.nextLine();
+        String fileName = scanner.nextLine();
+
+        String fileContent = "";
+        if (action.equals("PUT")) {
+            System.out.println("Enter file content:");
+            fileContent = scanner.nextLine();
+        }
+
+        String response = client.sendRequest(action + " " + fileName + " " + fileContent);
+
+        switch (action) {
+            case "PUT":
+                if (response.equals("200")) {
+                    System.out.println("The response says that the file was created!");
+                } else {
+                    System.out.println("The response says that creating the file was forbidden!");
+                }
+                break;
+                case "GET":
+                    if (response.equals("200")) {
+                        System.out.println("The content of the file is: " + response.substring(4));
+                    } else {
+                        System.out.println("The response says that the file was not found!");
+                    }
+                    break;
+                case "DELETE":
+                    if (response.equals("200")) {
+                        System.out.println("The response says that the file was successfully deleted!");
+                    } else {
+                        System.out.println("The response says that the file was not found!");
+                    }
+                    break;
+                default:
+                    System.out.println("Commmand not recognized");
+                    break;
+        }
+
+        client.stop();
+        scanner.close();
     }
 }
