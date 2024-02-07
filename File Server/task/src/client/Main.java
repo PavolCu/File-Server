abb5f7d116c60c120c7e3eaa60002fda0b0244dc
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-07 10:55:52 +0100

    started diffrend approach, started 3/4 stage

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -1,75 +1,32 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.SocketException;
-import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class Main {
+
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int PORT = 23456;
+
     public static void main(String[] args) {
-        try {
-            Client client = new Client("127.0.0.1", 23456);
-            client.start();
-            Scanner scanner = new Scanner(System.in);
 
-            while (true) {
-                System.out.println("Enter action (1 - get a file, 2 - create a file, 3 - delete a file):");
-                String action = scanner.nextLine();
+        try (
+                Socket socket = new Socket(InetAddress.getByName(SERVER_ADDRESS), PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        )
+        {
+            System.out.println("Client started!");
+            String wordsToServer = "Give me everything you have!";
+            output.writeUTF(wordsToServer);
+            System.out.println("Sent: " + wordsToServer);
+            String wordsFromServer = input.readUTF();
+            System.out.println("Received: " + wordsFromServer);
 
-                if ("exit".equals(action)) {
-                    client.sendExitCommand();
-                    System.out.println("The request was sent.");
-                    scanner.close();
-                    return;
-                }
-
-                System.out.println("Enter filename:");
-                String fileName = scanner.nextLine();
-
-                String fileContent = "";
-                String request;
-                String response;
-
-                switch (action) {
-                    case "1":
-                        request = "GET " + fileName;
-                        System.out.println("The request was sent.");
-                        response = client.sendRequest(request, " ");
-                        if (response.startsWith("200")) {
-                            System.out.println("The content of the file is: " + response.substring(4));
-                        } else {
-                            System.out.println("The response says that the file was not found!");
-                        }
-                        continue;
-                    case "2":
-                        System.out.println("Enter file content:");
-                        fileContent = scanner.nextLine();
-                        request = "PUT " + fileName + " " + fileContent;
-                        System.out.println("The request was sent.");
-                        response = client.sendRequest(request, fileContent);
-                        if (response == null) {
-                            System.out.println("Failed to get a response from the server.");
-                        } else if (response.startsWith("200")) {
-                            System.out.println("The response says that the file was created!");
-                        } else if (response.startsWith("403")) {
-                            System.out.println("The response says that creating the file was forbidden!");
-                        }
-                        continue;
-                    case "3":
-                        request = "DELETE " + fileName;
-                        System.out.println("The request was sent.");
-                        response = client.sendRequest(request, "");
-                        if (response.equals("200")) {
-                            System.out.println("The response says that the file was successfully deleted!");
-                        } else {
-                            System.out.println("The response says that the file was not found!");
-                         }
-                        continue;
-                    default:
-                        break;
-                }
-            }
         } catch (IOException e) {
-            System.out.println("Failed to start the client: " + e.getMessage());
             e.printStackTrace();
         }
     }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,11 +1,35 @@
 package server;
 
-public class Main {
-    public static void main(String[] args) {
-        Server server = new Server(23456);
-        server.start();
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-        // when you want to stop the server:
-        // server.stop();
+public class Main {
+
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int PORT = 23456;
+
+    public static void main(String[] args) {
+
+        System.out.println("Server started!");
+        try (
+                ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(SERVER_ADDRESS));
+                Socket socket = serverSocket.accept();
+        )
+        {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            String wordsFromClient = input.readUTF();
+            System.out.println("Received: " + wordsFromClient);
+            String wordsToClient = "All files were sent!";
+            output.writeUTF(wordsToClient);
+            System.out.println("Sent: " + wordsToClient);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
