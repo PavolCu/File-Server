6f9ff0874e504833aab566611191f3a9051f759a
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-07 17:00:54 +0100

    added logic to Main in client and server from Objectives

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
 
@@ -17,14 +18,60 @@ public class Main {
                 Socket socket = new Socket(InetAddress.getByName(SERVER_ADDRESS), PORT);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                Scanner scanner = new Scanner(System.in);
         )
         {
             System.out.println("Client started!");
-            String wordsToServer = "Give me everything you have!";
-            output.writeUTF(wordsToServer);
-            System.out.println("Sent: " + wordsToServer);
-            String wordsFromServer = input.readUTF();
-            System.out.println("Received: " + wordsFromServer);
+
+            System.out.println("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+            String action = scanner.nextLine();
+
+            String fileName = "";
+            String fileContent = "";
+
+            if (action.equals("exit")) {
+                System.out.println("The request was sent.");
+                output.writeUTF(action);
+                socket.close();
+                System.out.println("Disconnected from the server and terminated.");
+                return;
+            }
+
+            System.out.println("Enter file name:");
+            fileName = scanner.nextLine();
+            if (action.equals("2")) {
+                System.out.println("Enter the content of the file:");
+                fileContent = scanner.nextLine();
+            }
+
+
+            String requestToServer = action + " " + fileName + " " + fileContent;
+            output.writeUTF(requestToServer);
+
+            String responseFromServer = input.readUTF();
+            if (action.equals("2")) {
+                if (responseFromServer.equals("200")) {
+                    System.out.println("The response says that the file was created!");
+                } else {
+                    System.out.println("The response says that creating the file was forbidden!");
+                }
+            } else if (action.equals("1")) {
+                if (responseFromServer.startsWith("200 ")) {
+                    System.out.println("The content of the file is: " + responseFromServer.substring(4));
+                } else {
+                    System.out.println("The response says that the file was not found!");
+                }
+            } else if (action.equals("3")) {
+                if (responseFromServer.equals("200")) {
+                    System.out.println("The response says that the file was successfully deleted!");
+                } else {
+                    System.out.println("The response says that the file was not found!");
+                }
+            }
+            socket.close();
+            System.out.println("Disconnected from the server and terminated.");
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
@@ -15,19 +18,59 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Server started!");
+
         try (
                 ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(SERVER_ADDRESS));
-                Socket socket = serverSocket.accept();
         )
         {
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                )
+                {
+                    String requestFromClient = input.readUTF();
+                    String[] requestParts = requestFromClient.split(" ", 3);
+                    String action = requestParts[0];
+                    String fileName = requestParts[1];
+                    String fileContent = requestParts[2];
 
-            String wordsFromClient = input.readUTF();
-            System.out.println("Received: " + wordsFromClient);
-            String wordsToClient = "All files were sent!";
-            output.writeUTF(wordsToClient);
-            System.out.println("Sent: " + wordsToClient);
+                    if (action.equals("exit")) {
+                        break;
+                    }
+
+                    Path filePath = Paths.get("/Users/cuninkapavol/IdeaProjects/File Server/File Server/task/src/server/Data" + fileName);
+                    if (action.equals("2")) {
+                        if (Files.exists(filePath)) {
+                            output.writeUTF("403");
+                        } else {
+                            try {
+                                Files.write(filePath, fileContent.getBytes());
+                                output.writeUTF("200");
+                            } catch (IOException e) {
+                                output.writeUTF("403");
+                            }
+                        }
+                    } else if (action.equals("1")) {
+                        if (Files.exists(filePath)) {
+                            String content = new String(Files.readAllBytes(filePath));
+                            output.writeUTF("200 " + content);
+                        } else {
+                            output.writeUTF("404");
+                        }
+                    } else if (action.equals("3")) {
+                        if (Files.exists(filePath)) {
+                            Files.delete(filePath);
+                            output.writeUTF("200");
+                        } else {
+                            output.writeUTF("404");
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
