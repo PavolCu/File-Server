34adabe36249215bebf05e54ab933138407569bf
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-11 14:29:42 +0100

    2 tasks with executors

---
+++ Topics/Executors/Check whether a number is prime/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        while (scanner.hasNext()) {
+            int number = scanner.nextInt();
+            executor.submit(new PrintIfPrimeTask(number));
+        }
+
+        executor.shutdown();
+    }
+}
+
+class PrintIfPrimeTask implements Runnable {
+    private final int number;
+
+    public PrintIfPrimeTask(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        if (isPrime(number)) {
+            System.out.println(number);
+        }
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
